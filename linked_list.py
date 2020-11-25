3a8e9770afe89f69bb6907001cd9ebe8141d34f9
Bharath Vyas <bharathvyas07@gmail.com>
2020-11-25 21:31:27 +0530

    delete operation is added to the linked list

--- linked_list.py
+++ linked_list.py
@@ -18,7 +18,33 @@ class linked_list:
         temp.nextval=None
         self.tail.nextval=temp
         self.tail=temp
-        return
+        return    
+
+    def delete_node(self,index):
+        temp=self.head
+        i=0
+        invalid_flag=0
+        #locate the node
+        if(int(index) == 1):
+            self.head=temp.nextval
+            del temp
+            print("delete succcess" + self.head.dataval)
+            return
+        while(i!=int(index)-2):
+            temp=temp.nextval
+            if(temp.nextval == None):
+                invalid_flag=1
+                break
+            else:
+                i +=1
+        if (invalid_flag==1):
+            print("position out of range")
+            return
+        bag=temp.nextval
+        temp.nextval=bag.nextval
+        del bag
+        print("delete succcess")    
+        return     
 
     def printer(self):
         mapper=self.head
@@ -32,13 +58,16 @@ class linked_list:
 if __name__ == "__main__":
     linked = linked_list()
     choice=1
-    while(choice!=3):
+    while(choice!=4):
         if(choice==1):
             number = input("enter a number to add:")
             linked.new_node(number)
         if(choice==2):
             linked.printer()
-        print("1.add 2.print 3.exit")
+        if(choice==3):
+            number = input("enter position to be deleted:")
+            linked.delete_node(number)    
+        print("1.add 2.print 3.delete 4.exit")
         choice=int(input("enter a choice:"))  #the int is used because the input takes the vlaue as a character which messes up with if condition            
 
 
